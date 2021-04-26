bcfed5e50d27f9ec791a88712cd45f3cbc5f3908
Mateusz <mateusz.jablonski12@gmail.com>
2021-04-26 21:19:13 +0200

    p

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import pandas as pd
 
-x = pd.DataFrame({'Yes' : [50, 21], 'No':[131, 2]}, index=[''])
-print(x)
+# DataFrame
+
+
+x = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]}, index=['1', '2'])
+# print(x)
+
+y = pd.DataFrame({'Adam': ['Lubie pilke nozna', 'nie lubie golfa'],
+                  'Kordian': ['Nie lubie pilki noznej', 'lubie jezidz konno '], },
+                 index=['Opcja 1 ', 'Opcja 2 '])
+# print(y)
+
+# SERIES
+
+k = pd.Series([1, 2, 3, 4, 5])
+print(k)
+
+pd.Series([30, 35, 40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A')
