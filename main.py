deb7a2d715cc2b9a9c62ac9dd1373f2607b3a922
Mateusz <mateusz.jablonski12@gmail.com>
2021-04-26 21:40:50 +0200

    p

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ import pandas as pd
 # DataFrame
 
 
-x = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]}, index=['1', '2'])
+pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]}, index=['1', '2'])
 # print(x)
 
-y = pd.DataFrame({'Adam': ['Lubie pilke nozna', 'nie lubie golfa'],
+pd.DataFrame({'Adam': ['Lubie pilke nozna', 'nie lubie golfa'],
                   'Kordian': ['Nie lubie pilki noznej', 'lubie jezidz konno '], },
                  index=['Opcja 1 ', 'Opcja 2 '])
 # print(y)
 
 # SERIES
 
-k = pd.Series([1, 2, 3, 4, 5])
-print(k)
+pd.Series([1, 2, 3, 4, 5])
 
-pd.Series([30, 35, 40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A')
+#print(k)
+
+x = pd.Series([30, 35, 40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A')
+#print(x)
+
+wine_reviews = pd.read_csv("housing.csv")
+
+print(wine_reviews.head())
+
+print(wine_reviews.shape)
+
